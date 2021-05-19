b062da04f13764f39b8ae23b5d01a691d2d43cb5
victorathanasio <victorathanasio@usp.br>
2021-05-19 21:59:15 +0200

    Implement List investments filtered by ville and/or by etat_d_avancement

--- REST_API/rest_api.py
+++ REST_API/rest_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import pandas as pd
 import json
 # import requests
@@ -18,16 +18,34 @@ import json
 
 API = Blueprint('simple_page', __name__)
 
-@API.route('/API/v1/all', methods=['GET'])
+@API.route('/API/v1/list_all', methods=['GET'])
 def get_all():
     """
     Function that returns all investments in JSON format.
-    :return: 
+    :return: json with all investments
     """
     with open('dataset.json') as json_file:
         data = json.load(json_file)
     return jsonify(data)
 
+# List investments filtered by ville and/or by etat_d_avancement
+
+@API.route('/API/v1/list', methods=['GET'])
+def get_from_base():
+    """
+    Function that allows to filter by ville and/or etat_d_avancement
+    To filter, use the ville and etat_d_avancement as URL parameters
+    :return: json that contains the filtered data
+    """
+    args = request.args
+    data = pd.read_json("dataset.json")
+    print(data.shape)
+    if 'ville' in args:
+        data = data.drop(data[data['ville']!=args['ville']].index)
+    if 'etat_d_avancement' in args:
+        data = data.drop(data[data['etat_d_avancement']!=args['etat_d_avancement']].index)
+    data = data.dropna(axis=1)
+    return jsonify(data.to_dict('records'))
 
 
 # @simple.route('/', methods=['GET'])
