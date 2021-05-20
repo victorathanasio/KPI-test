735942bff87205c387bcd89a253a30edadef51c5
victorathanasio <victorathanasio@usp.br>
2021-05-20 05:34:06 +0200

    final arrangments

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from WebApp.Layout import Layout
 
 app.layout = Layout()
 app.server.register_blueprint(API)
+server = app.server
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=90)
+    # app.run_server(debug=False, host='0.0.0.0', port=90)
+    app.run_server(debug=True)
